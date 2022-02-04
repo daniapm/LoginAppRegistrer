827b0a96a5ab550ea5e1dfa53f0edf6753ac3901
daniapm <daniajibellis@hotmail.com>
2022-02-04 15:42:52 -0600

    Adding USeCase to Domain

--- app/src/main/java/com/example/loginappregistrer/domain/repository/LogRegisterRepository.kt
+++ app/src/main/java/com/example/loginappregistrer/domain/repository/LogRegisterRepository.kt
@@ -1,7 +1,9 @@
 package com.example.loginappregistrer.domain.repository
 
 import com.example.loginappregistrer.domain.model.Users
+import com.example.loginappregistrer.utils.Result
+import java.lang.Exception
 
 interface LogRegisterRepository {
-    suspend fun getUsers(): Result<List<Users>>
+    suspend fun getUsers(): Result<List<Users>, Exception>
 }
